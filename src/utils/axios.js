1d375336c2e120f09342e6af47cf1c7901a987b7
sandjonyves <sandjonyves@gmail.com>
2024-12-20 08:35:00 +0000

    change base url

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -3,8 +3,8 @@ import axios from "axios";
 const Axios = axios.create({
     // baseURL:"http://127.0.0.1:8000"
     // baseURL:"https://e-commerce-backend-qmkw.onrender.com/"
-    // baseURL:"https://rqaxyn5max.us-east-1.awsapprunner.com/"
-    baseURL : 'http://127.0.0.1:8000'
+    baseURL:"https://rqaxyn5max.us-east-1.awsapprunner.com/"
+    // baseURL : 'http://127.0.0.1:8000'
 
 })
 
